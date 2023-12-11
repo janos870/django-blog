cb4063a08bfa98342c8b7f913629dcf5398a98b0
janos870 <janos.balogh870@gmail.com>
2023-12-11 10:15:41 +0100

    update last commit

--- blog/forms.py
+++ blog/forms.py
@@ -10,6 +10,6 @@ class CommentForm(forms.Form):
     )
     body = forms.CharField(
         widget=forms.TextInput(
-            attrs={"class": "form-control shadow-none", "placeholder": "Leave a comment!"}
+            attrs={"class": "form-control shadow-none", "placeholder": "Leave a comment!", "row": 1}
         )
     )
